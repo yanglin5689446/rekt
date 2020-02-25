d98bddeec030bc70b733f14ec0ce92aa16152f26
Yang Lin <yanglin5689446@gmail.com>
2020-02-25 17:19:38 +0800

    concurrent mode

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function createElement(type, props, ...children) {
     type,
     props: {
       ...props,
-      children: children.map(child => 
+      children: children.map(child =>
         typeof child === "object"
           ? child
           : createTextElement(child)
@@ -23,21 +23,89 @@ function createElement(type, props, ...children) {
   }
 }
 
-function render(element, container) {
-  const dom = element.type === "TEXT_ELEMENT"
+function createDom(fiber) {
+  const dom = fiber.type === "TEXT_ELEMENT"
     ? document.createTextNode("")
-    : document.createElement(element.type)
+    : document.createElement(fiber.type)
 
   const isProperty = key => key !== "children"
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isProperty)
     .forEach(name => {
-      dom[name] = element.props[name]
+      dom[name] = fiber.props[name]
     })
 
-  element.props.children.forEach(child => render(child, dom))
+  return dom
+}
 
-  container.appendChild(dom)
+function render(element, container) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element]
+    }
+  }
+}
+
+let nextUnitOfWork = null
+
+function workLoop(deadline) {
+  let shouldYield = false
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  requestIdleCallback(workLoop)
+}
+
+requestIdleCallback(workLoop)
+
+function performUnitOfWork(fiber) {
+  // add dom node
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom)
+  }
+
+  // create new fibers
+  const elements = fiber.props.children
+  let index = 0
+  let prevSibling = null
+
+  while (index < elements.length) {
+    const element = elements[index]
+
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    }
+
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevSibling.sibling = newFiber
+    }
+
+    prevSibling = newFiber
+    index ++
+  }
+
+  // return next unit of work
+  if (fiber.child) {
+    return fiber.child
+  }
+  let nextFiber = fiber
+  while (nextFiber) {
+    if (nextFiber.sibling){
+      return nextFiber.sibling
+    }
+    nextFiber = nextFiber.parent
+  }
 }
 
 const Rekt = {
